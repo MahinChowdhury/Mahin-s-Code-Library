c33d9c48c737454f7acd5131fa4f5b98e9e79739
Mahin <mahin0021@gmail.com>
2022-02-13 19:25:30 +0600

    first commit

---
+++ Searching/Binary Search/upper_bound.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+ 
+using namespace std;
+#define ll long long
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+int BS(vector<int>a,int n,int tar){
+    
+    int lo=0,hi=n,ans=-1;
+    
+    while(lo<hi){
+        int mid = lo + (hi-lo)/2;
+        if(a[mid]>tar){
+            hi = mid;
+        }
+        else{
+            lo = mid+1;
+            ans = mid;
+        }
+    }
+    return ans;
+
+}
+
+void solve(){
+
+    //Start writing your code from here.
+
+}
+int main(){
+    
+    IOS;
+    int tc=1;
+    //cin>>tc;
+    while(tc--){
+        solve();
+    }
+ 
+}
